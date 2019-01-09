685e7f99f816e7447fc48c9dcfe7fd0b0bc2957a
dan-76 <dc29183@connect.hku.hk>
2019-01-09 23:06:36 +0800

    Add about us page and corresponding testing
    make use of base.html to create multiple pages including home and about us, this includes a nav bar

--- booktime/main/tests.py
+++ booktime/main/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
 class TestPage(TestCase):
@@ -6,4 +7,10 @@ class TestPage(TestCase):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home.html')
+        self.assertContains(response, 'BookTime')
+    
+    def test_about_us_page_works(self):
+        response = self.client.get(reverse("about_us"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'about_us.html')
         self.assertContains(response, 'BookTime')
