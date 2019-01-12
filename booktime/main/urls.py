00d9cbabe3e4454c61a56aca7408f6b643db41ce
dan-76 <dc29183@connect.hku.hk>
2019-01-12 12:53:31 +0800

    Add contact us form and corresponding testing
    make use of base.html to create contact us form, and also use the bootstrap as a css framework for better form design. by contact us, make use of Django e-mail library and logging

--- booktime/main/urls.py
+++ booktime/main/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from main import views
+
 
 urlpatterns = [
+    path(
+        "contact-us/",
+        views.ContactUsView.as_view(),
+        name="contact_us",
+    ),
     path(
         'about-us/',
         TemplateView.as_view(template_name="about_us.html"),
