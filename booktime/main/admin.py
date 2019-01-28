410b4c7063d51439dcaf45c1a079c7b7b1aa673d
dan-76 <dc29183@connect.hku.hk>
2019-01-28 22:30:52 +0800

    Reporting on Orders

--- booktime/main/admin.py
+++ booktime/main/admin.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import (
     UserAdmin as DjangoUserAdmin
 )
+from django import forms
 from django.utils.html import format_html
 from django.db.models.functions import TruncDay
 from django.db.models import Avg, Count, Min, Sum
@@ -287,6 +288,13 @@ class ColoredAdminSite(admin.sites.AdminSite):
         return context
 
 
+class PeriodSelectForm(forms.Form):
+    PERIODS = ((30, "30 days"), (60, "60 days"), (90, "90 days"))
+    period = forms.TypedChoiceField(
+        choices=PERIODS, coerce=int, required=True
+    )
+
+
 # The following will add reporting views to the list of
 # available urls and will list them from the index page
 class ReportingColoredAdminSite(ColoredAdminSite):
@@ -296,7 +304,12 @@ class ReportingColoredAdminSite(ColoredAdminSite):
             path(
                 "orders_per_day/",
                 self.admin_view(self.orders_per_day),
-            )
+            ),
+            path(
+                "most_bought_products/",
+                self.admin_view(self.most_bought_products),
+                name="most_bought_products",
+            ),
         ]
         return my_urls + urls
     
@@ -326,12 +339,52 @@ class ReportingColoredAdminSite(ColoredAdminSite):
             request, "orders_per_day.html", context
         )
     
+    def most_bought_products(self, request):
+        if request.method == "POST":
+            form = PeriodSelectForm(request.POST)
+            if form.is_valid():
+                days = form.cleaned_data["period"]
+                starting_day = datetime.now() - timedelta(
+                    days=days
+                )
+                data = (
+                    models.OrderLine.objects.filter(
+                        order__date_added__gt=starting_day
+                    )
+                    .values("product__name")
+                    .annotate(c=Count("id"))
+                )
+                logger.info(
+                    "most_bought_products query: %s", data.query
+                )
+                labels = [x["product__name"] for x in data]
+                values = [x["c"] for x in data]
+        else:
+            form = PeriodSelectForm()
+            labels = None
+            values = None
+        
+        context = dict(
+            self.each_context(request),
+            title="Most bought products",
+            form=form,
+            labels=labels,
+            values=values,
+        )
+        return TemplateResponse(
+            request, "most_bought_products.html", context
+        )
+
     def index(self, request, extra_context=None):
         reporting_pages = [
             {
                 "name": "Orders per day",
                 "link": "orders_per_day/",
-            }
+            },
+            {
+                "name": "Most bought products",
+                "link": "most_bought_products/",
+            },
         ]
         if not extra_context:
             extra_context = {}
